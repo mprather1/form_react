769be1d2b7622c52809bc1f2615855be7b9565b2
Michael Prather <mprather1@gmail.com>
2016-10-09 22:17:48 -0500

    form

--- app/components/EmailField.jsx
+++ app/components/EmailField.jsx
@@ -3,22 +3,25 @@ var validator = require("email-validator");
 
 var EmailField = React.createClass({
   getInitialState: function(){
-    return {valid: true, value: ""}
+    return {valid: true, value: ""};
   },
   onChange: function(e){
     var val = e.target.value;
     
     if (!validator.validate(e.target.value)){
-      this.setState({valid: false, value: e.target.value})
+      this.setState({valid: false, value: e.target.value});
     } else {
-      this.setState({valid: true, value: e.target.value})
+      this.setState({valid: true, value: e.target.value});
     }
   },
+  clear: function(){
+    this.setState({valid: true, value: ""});
+  },
   render: function(){
-    var formClass = this.state.valid ? "form-group" : "form-grout has-error"
+    var formClass = this.state.valid ? "form-group" : "form-grout has-error";
     return (
       <div className={formClass}>
-        <input className="form-control" onChange={this.onChange} placeholder="Email..." value={this.state.value} />
+        <input className="form-control" onChange={this.onChange} placeholder="Email" value={this.state.value} />
       </div>
     );
   }
